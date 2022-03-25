dbb414221e87aa7b5242695125399c6ed5ea4b18
Rodrigo <rodrigorael53@gmail.com>
2022-03-25 17:09:06 -0300

    consumindo uma API da forma simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,33 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+type Repository = {
+  full_name: string;
+  description: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
+  useEffect(() => {
+    fetch('https://api.github.com/users/Rodrigo001-de/repos')
+      .then(response => response.json())
+      .then(data => {
+        setRepositories(data);
+      });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <ul>
+      {repositories.map(repo => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
-export default App
+export default App;
